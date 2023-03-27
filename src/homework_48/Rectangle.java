712782056f5f555c4e4cc9d88a79264426253981
Leon Zuev <loonatune@gmail.com>
2023-03-27 18:53:16 +0300

    Add comparator, body rectangle

--- src/homework_48/Rectangle.java
+++ src/homework_48/Rectangle.java
@@ -4,4 +4,18 @@ public class Rectangle {
 
   private final int length;
   private final int width;
+
+  public Rectangle(int a, int b) {
+    // ширина всегда меньше длинны
+    length = Math.max(a, b);
+    width = Math.min(a, b);
+  }
+  public int getWidth() {
+    return width;
+  }
+
+  @Override
+  public String toString() {
+    return "Rectangle {длина";
+  }
 }
