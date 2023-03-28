aa66cb5080fa5d2f8b58ff7e632aba564c9d46cb
Leon Zuev <loonatune@gmail.com>
2023-03-28 17:56:47 +0300

    Rename Main to Age

--- src/Age.java
+++ src/Age.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class Main {
+public class Age {
 
   public static void main(String[] args) throws IOException {
     // Date - количество миллисекунд с момента 00:00:00 UTC 01.01.1970
